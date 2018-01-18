b27f69a6b98e22f259a440b050c8a024e47c8a00
Clément Vion <clement.vion49@gmail.com>
2018-01-18 11:03:10 +0100

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+}
+</style>
---
+++ pages/projects/_slug.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <section class="container">
+    <h1>{{ data.name }}</h1>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  asyncData({ params, error }) {
+    return axios.get(`http://localhost:3000/data/projects/${params.slug}.json`)
+    .then((res) => {
+      return {
+        data: res.data,
+      }
+    })
+    .catch((e) => {
+      error({ statusCode: 404, message: 'Project not found' })
+    })
+  },
+  mounted() {
+    console.log(this.data.slug);
+  }
+}
+</script>
+
+<style lang="css">
+</style>
